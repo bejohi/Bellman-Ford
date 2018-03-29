837b1996b6c9dbe6171c162a21ef8b5496db7f32
Jonas Hielscher <benedikt.hielscher@st.ovgu.de>
2018-03-29 23:15:12 +0200

    ADDED: A function to add edges.

--- src/graph.h
+++ src/graph.h
@@ -2,41 +2,57 @@
 #define INF236_CA2_ADJMATRIX_H
 
 #include <stdlib.h>
+#include <math.h>
 
 #define maxMatrixSize 10000
+#define noEdge -0
 
 typedef struct Graph {
     unsigned int size;
-    long long** adjMatrix;
+    double **adjMatrix;
+    bool isBidirectional;
 
 } Graph;
 
 
-Graph createGraph(unsigned int numberOfVertices){
-    if(numberOfVertices > maxMatrixSize){
+Graph createGraph(unsigned int numberOfVertices) {
+    if (numberOfVertices > maxMatrixSize) {
         numberOfVertices = maxMatrixSize;
     }
     Graph graph = {};
     graph.size = numberOfVertices;
-    graph.adjMatrix = (long long**) malloc(numberOfVertices * sizeof(long long*));
-    for(unsigned int i = 0; i < numberOfVertices; i++){
-        graph.adjMatrix[i] = (long long*) malloc(numberOfVertices * sizeof(long long));
-        for(unsigned int x = 0; x < numberOfVertices; x++){
-            graph.adjMatrix[i][x] = 0;
+    graph.adjMatrix = (double **) malloc(numberOfVertices * sizeof(double *));
+    for (unsigned int i = 0; i < numberOfVertices; i++) {
+        graph.adjMatrix[i] = (double *) malloc(numberOfVertices * sizeof(double));
+        for (unsigned int x = 0; x < numberOfVertices; x++) {
+            graph.adjMatrix[i][x] = noEdge;
         }
     }
+    graph.isBidirectional = true;
     return graph;
 }
 
-void destroyGraph(Graph* graph){
-    for(unsigned int i = 0; i < graph->size; i++){
-        if(graph->adjMatrix[i] != NULL){
+void destroyGraph(Graph *graph) {
+    for (unsigned int i = 0; i < graph->size; i++) {
+        if (graph->adjMatrix[i] != NULL) {
             free(graph->adjMatrix[i]);
         }
     }
-    if(graph->adjMatrix != NULL){
+    if (graph->adjMatrix != NULL) {
         free(graph->adjMatrix);
     }
 }
+
+void addEdge(Graph *graph, unsigned int vertex1, unsigned int vertex2, double weight) {
+    if (graph->size <= vertex1 || graph->size <= vertex2) {
+        return;
+    }
+    graph->adjMatrix[vertex1][vertex2] = weight;
+
+    if (graph->isBidirectional) {
+        graph->adjMatrix[vertex2][vertex1] = weight;
+    }
+}
+
 
 #endif //INF236_CA2_ADJMATRIX_H
--- src/testGraph.c
+++ src/testGraph.c
@@ -5,22 +5,22 @@
 
 static unsigned int testCounter = 0;
 
-static bool test_createGraph_sizeIs10000_graphCorrectCreated(){
+static bool test_createGraph_sizeIs10000_graphCorrectCreated() {
     // Arrange
     unsigned int size = 10000;
     // Act
     Graph graph = createGraph(size);
 
     // Assert
-    if(graph.size != size){
+    if (graph.size != size) {
         return false;
     }
 
-    if(graph.adjMatrix[0][0] != 0){
+    if (graph.adjMatrix[0][0] != noEdge) {
         return false;
     }
 
-    if(graph.adjMatrix[size-1][size-1] != 0){
+    if (graph.adjMatrix[size - 1][size - 1] != noEdge) {
         return false;
     }
 
@@ -28,6 +28,22 @@ static bool test_createGraph_sizeIs10000_graphCorrectCreated(){
     return true;
 }
 
+
+static bool test_addEdge_edgeIsInBounds_EdgeAdded() {
+    // Arrange
+    Graph graph = createGraph(10);
+
+    // Act
+    addEdge(&graph, 5, 4, 10.5);
+
+    // Assert
+    if (graph.adjMatrix[5][4] != 10.5 || graph.adjMatrix[4][5] != 10.5) {
+        return false;
+    }
+
+    destroyGraph(&graph);
+    return true;
+}
 
 static void printTestResult(bool result, char *testName) {
     if (!result) {
@@ -41,5 +57,6 @@ void runTestSuite() {
 
     printTestResult(test_createGraph_sizeIs10000_graphCorrectCreated(),
                     "test_createGraph_sizeIs10000_graphCorrectCreated");
+    printTestResult(test_addEdge_edgeIsInBounds_EdgeAdded(), "test_addEdge_edgeIsInBounds_EdgeAdded");
     printf("%d tests run \n", testCounter);
 }
