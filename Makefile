6a46b1901cb369942c49c311506f50ee64d3afdd
Jonas Hielscher <benedikt.hielscher@st.ovgu.de>
2018-04-01 23:35:56 +0200

    ADDED: A base CUDA structure.

--- Makefile
+++ Makefile
@@ -3,6 +3,13 @@
 all: build
 	./bin/bellmanFord
 
+gpu: gbu-build
+	./bin/bellmanFordCuda
+
+gbu-build: prep
+	nvcc -o bin/bellmanFordCuda src/completeGraph.c src/bellmanFordCompleteGraphGpuParallel.c -lm -O3 -Wall -Wpedantic
+
+
 build: prep
 	gcc -o bin/bellmanFord src/completeGraph.c src/bellmanFordCompleteGraphCpuParallel.c src/bellmanFordCompleteGraphSequential.c src/testBellmanForcCompleteGraphSequential.c src/testCompleteGraph.c src/reportTools.c src/main.c -std=c99 -fopenmp -lm -O3 -Wall -Wpedantic
 
---
+++ src/bellmanFordCompleteGraphGpuParallel.cu
@@ -0,0 +1,28 @@
+#include "bellmanFordCompleteGraphGpuParallel.h"
+
+double bellmanFord(CompleteGraph *graph, unsigned int startVertex) {
+    if (!graph || !graph->adjMatrix || !graph->predecessor || !graph->dist) {
+        return -1;
+    }
+    initArrays(graph->dist, graph->predecessor, graph->size);
+    graph->dist[startVertex] = 0;
+    double starttime, endtime;
+    bool finished;
+    for (unsigned int n = 0; n < graph->size; n++) {
+        finished = true;
+        for (unsigned int y = 0; y < graph->size; y++) {
+            for (unsigned int x = 0; x < graph->size; x++) {
+                float weight = graph->adjMatrix[y][x];
+                if (graph->dist[y] + weight < graph->dist[x]) {
+                    graph->dist[x] = graph->dist[y] + weight;
+                    graph->predecessor[x] = y;
+                    finished = false;
+                }
+            }
+        }
+        if(finished){
+            break;
+        }
+    }
+    return -1;
+}
