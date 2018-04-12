288229c1ee2ed663932a7ec2fd4279cd2cf3fc3c
Jonas Hielscher <benedikt.hielscher@st.ovgu.de>
2018-04-12 11:37:05 +0200

    CHANGEND: correct CSV output.

--- src/bellmanFordCompleteGraphGpuParallel.cu
+++ src/bellmanFordCompleteGraphGpuParallel.cu
@@ -283,12 +283,12 @@ static void createReport() {
     GpuGraph gpuGraph = createGpuGraph(n);
     fillGpuGraphRandom(&gpuGraph);
     bellmanFord(&cpuGrap, 0);
-    printf("# ...done start outer loop.\n");
+    printf("# ...done! Run test cases...\n");
     for (unsigned int tPtr = 0; tPtr < 11; tPtr++) {
         for (unsigned int bPtr = 0; bPtr < 7; bPtr++) {
             double time = bellmanFordGpu(&gpuGraph, 0, blockArr[bPtr], threadArr[tPtr]);
             bool check = cmpDistArr(&cpuGrap, &gpuGraph,gpuGraph.size);
-            printf("parallelGpu;n=%d;threads=%d;blockSize=%d;time=%lf;check=%d\n", n, threadArr[tPtr],
+            printf("parallelGpu;n=%d;threads=%d;blockSize=%d;time=%lf;check=%d;\n", n, threadArr[tPtr],
                    blockArr[bPtr], time, check);
         }
     }
